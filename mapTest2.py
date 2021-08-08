29b28175cbe26782584a60ea609e830d94194c1f
NagomuAmane <mikugoo39@gmail.com>
2021-08-08 18:44:50 +0900

    関数をまとめた。

--- mapTest2.py
+++ mapTest2.py
@@ -5,7 +5,7 @@ from OpenGL.GL import*
 from PIL import Image
 
 
-def display():
+def display(window):
     maplist = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
     glClear(GL_COLOR_BUFFER_BIT)
 
@@ -59,15 +59,16 @@ def display():
     glDisable(GL_BLEND)
 
     glfw.swap_buffers(window)
-
     glFlush()
 
-def init():
+
+def init(window):
     glClearColor(0.0,0.0,1.0,1.0)
-    display()
+    display(window)
 
 def window_refresh(window):
-    display()
+    display(window)
+    
 
 def load_texture():
     map1_path = os.path.join(os.path.dirname(__file__), u"data\\TowaruIcon.png")
@@ -75,32 +76,36 @@ def load_texture():
     w,h = img.size
     glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img.tobytes())
 
-if not glfw.init():
-    raise RuntimeError('Could not initialize GLFW3')
+def main():
+    if not glfw.init():
+        raise RuntimeError('Could not initialize GLFW3')
 
-window = glfw.create_window(800,800,'mapTest',None,None)
+    window = glfw.create_window(800,800,'mapTest',None,None)
 
-if not window:
+    if not window:
+        glfw.terminate()
+        raise RuntimeError('Could not create window')
+
+    glfw.set_window_refresh_callback(window,window_refresh)
+    glfw.make_context_current(window)
+
+    load_texture()
+    init(window)
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
+
+
+    while not glfw.window_should_close(window):
+        glfw.wait_events()
+        err = glGetError()
+        if err != GL_NO_ERROR:
+            print(err)
+
+    window_refresh(window)
     glfw.terminate()
-    raise RuntimeError('Could not create window')
 
-glfw.set_window_refresh_callback(window,window_refresh)
-glfw.make_context_current(window)
-
-load_texture()
-init()
-glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
-glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
-
-
-while not glfw.window_should_close(window):
-    glfw.wait_events()
-    err = glGetError()
-    if err != GL_NO_ERROR:
-        print(err)
-
-window_refresh(window)
-glfw.terminate()
+if __name__=="__main__":
+    main()
 
 
 
