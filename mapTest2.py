4c7e8006cff5090b0bc6c33dee29ad948b59da22
NagomuAmane <mikugoo39@gmail.com>
2021-08-08 23:05:58 +0900

    複数種類のテクスチャが貼れん!!!!!! 読み込みとか格納のしかたから見直す必要ありそう。

--- mapTest2.py
+++ mapTest2.py
@@ -3,10 +3,33 @@ import sys
 import os
 from OpenGL.GL import*
 from PIL import Image
+texID = []
+img0 = None
+img1 = None
 
+class maps():
+    def drawMap():
+        maplist = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+        
+        #0から4
+        for i in range(5):
+            for j in range(5):
+                if maplist[j][i]==0:
+                    glBindTexture(GL_TEXTURE_2D, img0)
+                else:
+                    glBindTexture(GL_TEXTURE_2D, img1)
+                glBegin(GL_QUADS)
+                glTexCoord2d(0.0,1.0)#画像左下
+                glVertex2d(0.0+j*32,32+i*32)
+                glTexCoord2d(0.0,0.0)#画像左上
+                glVertex2d(0.0+j*32,0.0+i*32)
+                glTexCoord2d(1.0,0.0)#画像右上
+                glVertex2d(32+j*32,0.0+i*32)
+                glTexCoord2d(1.0,1.0)#画像右下
+                glVertex2d(32+j*32, 32+i*32)
+                glEnd()
 
 def display(window):
-    maplist = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
     glClear(GL_COLOR_BUFFER_BIT)
 
     #描画範囲の設定と原点の設定
@@ -26,19 +49,8 @@ def display(window):
     
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
-
-
-    glBegin(GL_QUADS)
-    glTexCoord2d(0.0,1.0)#画像左下
-    glVertex2d(0.0,32)
-    glTexCoord2d(0.0,0.0)#画像左上
-    glVertex2d(0.0,0.0)
-    glTexCoord2d(1.0,0.0)#画像右上
-    glVertex2d(32,0.0)
-    glTexCoord2d(1.0,1.0)#画像右下
-    glVertex2d(32, 32)
     
-    glEnd()
+    maps.drawMap()
 
     #test
     """
@@ -71,10 +83,17 @@ def window_refresh(window):
     
 
 def load_texture():
-    map1_path = os.path.join(os.path.dirname(__file__), u"data\\TowaruIcon.png")
-    img = Image.open(map1_path,'r')
-    w,h = img.size
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img.tobytes())
+    map0_path = os.path.join(os.path.dirname(__file__), u"data\\kusa.png")
+    map1_path = os.path.join(os.path.dirname(__file__), u"data\\tile.png")
+    img0 = Image.open(map0_path,'r')
+    img1 = Image.open(map1_path,'r')
+    w = 32
+    h = 32
+    #テクスチャIDわからん!!!!
+    #メモリまわりもう少し勉強いる?調べる。
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img0.tobytes())
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img1.tobytes())
+    #glBindTexture(GL_TEXTURE_2D, texID)
 
 def main():
     if not glfw.init():
@@ -94,7 +113,7 @@ def main():
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
 
-
+    #ゲームループ
     while not glfw.window_should_close(window):
         glfw.wait_events()
         err = glGetError()
