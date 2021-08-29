613053109ee60d5e1b90d6f9d7ef25f464c4ea9b
NagomuAmane <mikugoo39@gmail.com>
2021-08-29 16:16:50 +0900

    ゲームフェーズの追加

--- mapTest2.py
+++ mapTest2.py
@@ -3,7 +3,8 @@ import sys
 import os
 from OpenGL.GL import*
 from PIL import Image
-texID = []
+window_width = 800
+window_height = 800
 
 
 def keyboard(window, key, scancode, action, mods):
@@ -36,9 +37,9 @@ class maps():
         for i in range(7):
             for j in range(7):
                 if maplist[i][j]==0:
-                    glBindTexture(GL_TEXTURE_2D, 1)
+                    glBindTexture(GL_TEXTURE_2D, texture.texID[0])
                 else:
-                    glBindTexture(GL_TEXTURE_2D, 2)
+                    glBindTexture(GL_TEXTURE_2D, texture.texID[1])
                 glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
                 glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)   
                 glBegin(GL_QUADS)
@@ -55,18 +56,18 @@ class maps():
 class player():
 
     def drawPlayer():
-        glBindTexture(GL_TEXTURE_2D, 3)
+        glBindTexture(GL_TEXTURE_2D, texture.texID[2])
         glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)   
         glBegin(GL_QUADS)
         glTexCoord2d(0.0,1.0)#画像左下
-        glVertex3d(0.0,128,1.0)
+        glVertex3d(window_width/2-64,window_height/2-64+128,1.0)
         glTexCoord2d(0.0,0.0)#画像左上
-        glVertex3d(0.0,0.0,1.0)
+        glVertex3d(window_width/2-64,window_height/2-64+0.0,1.0)
         glTexCoord2d(1.0,0.0)#画像右上
-        glVertex3d(128,0.0,1.0)
+        glVertex3d(window_width/2-64+128,window_height/2-64+0.0,1.0)
         glTexCoord2d(1.0,1.0)#画像右下
-        glVertex3d(128,128,1.0)
+        glVertex3d(window_width/2-64+128,window_height/2-64+128,1.0)
         glEnd()
 
 def display(window):
@@ -109,34 +110,38 @@ def init(window):
 def window_refresh(window):
     display(window)
     
+class texture():
+    texID = []
+    def load_texture():
+        map0_path = os.path.join(os.path.dirname(__file__), u"data\\kusa.png")
+        map1_path = os.path.join(os.path.dirname(__file__), u"data\\tile.png")
+        player_path = os.path.join(os.path.dirname(__file__), u"data\\neko.png")
 
-def load_texture():
-    map0_path = os.path.join(os.path.dirname(__file__), u"data\\kusa.png")
-    map1_path = os.path.join(os.path.dirname(__file__), u"data\\tile.png")
-    map2_path = os.path.join(os.path.dirname(__file__), u"data\\neko.png")
-
-    img0 = Image.open(map0_path,'r')
-    img1 = Image.open(map1_path,'r')
-    img2 = Image.open(map2_path,'r')
-    texID=glGenTextures(3)
-    w = 64
-    h = 64
-    
-    #テクスチャの生成と文字列変換
-    glBindTexture(GL_TEXTURE_2D, 1)
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img0.tobytes())
-    glBindTexture(GL_TEXTURE_2D, 2)
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img1.tobytes())
-    glBindTexture(GL_TEXTURE_2D, 3)
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img2.tobytes())
-    
-    
+        img0 = Image.open(map0_path,'r')
+        img1 = Image.open(map1_path,'r')
+        img2 = Image.open(player_path,'r')
+        texture.texID=glGenTextures(3)
+        w = 64
+        h = 64
+        
+        #テクスチャの生成と文字列変換
+        glBindTexture(GL_TEXTURE_2D, texture.texID[0])
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img0.tobytes())
+        glBindTexture(GL_TEXTURE_2D, texture.texID[1])
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img1.tobytes())
+        glBindTexture(GL_TEXTURE_2D, texture.texID[2])
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,img2.tobytes())
+        
+class phase_ctrl():
+    gamephase = 0
+    def phase_change(number):
+        gamephase = number
 
 def main():
     if not glfw.init():
         raise RuntimeError('Could not initialize GLFW3')
 
-    window = glfw.create_window(800,800,'mapTest',None,None)
+    window = glfw.create_window(window_width,window_height,'mapTest',None,None)
 
     if not window:
         glfw.terminate()
@@ -146,9 +151,8 @@ def main():
     glfw.make_context_current(window)
 
     
-    load_texture()
+    texture.load_texture()
     init(window)
-    #glfw.set_mouse_button_callback(window, mouse_button)
     glfw.set_key_callback(window, keyboard)
 
     #ゲームループ
